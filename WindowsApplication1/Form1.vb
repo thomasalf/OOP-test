56ede0cb787a6ac28390b9c2357b55626479981b
ronningmar <ronningmar@gmail.com>
2015-02-23 20:14:29 +0100

    lagt til label

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -1,3 +1,10 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
 End Class
