8cdd552706ffd94da777ce8237998674e1bca0a2
tovestoedle <tove_69@hotmail.com>
2015-04-09 18:23:25 +0200

    kobling til database
    Lagt til 2 Imports og funksjon for kobling til database i Form1.

--- WindowsApplication8/Form1.vb
+++ WindowsApplication8/Form1.vb
@@ -1,4 +1,27 @@
-﻿Public Class Form1
+﻿Imports MySql.Data.MySqlClient
+Imports System.Configuration
+
+Public Class Form1
+
+    'Funksjon for kobling til database
+    Private Function query(sql As String) As DataTable
+        Dim data As New DataTable
+        Dim conn As MySqlConnection = New MySqlConnection
+        conn.ConnectionString = ConfigurationManager.ConnectionStrings("mysql").ConnectionString
+        Try
+            conn.Open()
+            Dim adapter As New MySqlDataAdapter
+            adapter.SelectCommand = New MySqlCommand(sql, conn)
+            adapter.Fill(Data)
+            conn.Close()
+        Catch ex As Exception
+            MessageBox.Show("Feil ved oppkobling til database: " & ex.Message)
+        Finally
+            conn.Dispose()
+        End Try
+        Return Data
+    End Function
+
 
     Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
         TabControl1.SelectTab(7)
@@ -8,7 +31,7 @@
         TabControl1.SelectTab(1)
     End Sub
 
-  
+
     Private Sub Side3ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Side3ToolStripMenuItem.Click
         TabControl1.SelectTab(2)
     End Sub
@@ -18,12 +41,12 @@
     End Sub
 
 
-   
+
     Private Sub ToolStripMenuItem5_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem5.Click
         TabControl1.SelectTab(3)
     End Sub
 
-   
+
 
     Private Sub UtstyrToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UtstyrToolStripMenuItem.Click
         TabControl1.SelectTab(4)
