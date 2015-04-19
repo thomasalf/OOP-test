3e3620999db096f173a2be479ff790187de7c0b6
ronningmar <ronningmar@gmail.com>
2015-04-19 12:26:57 +0200

    19.4.15

--- WindowsApplication8/Form1.vb
+++ WindowsApplication8/Form1.vb
@@ -284,6 +284,23 @@ Public Class Form1
     End Sub
 
     Private Sub Button21_Click(sender As Object, e As EventArgs) Handles Button21.Click
+        Dim sykkelnavn As String = ComboBox1.SelectedItem
+        Dim status As String = ComboBox4.SelectedItem
+        Dim data As New DataTable
+
+        Dim sqlHjelp As String = status.Substring(0, status.IndexOf(" "))
+        'LAger variabel med bare sykkelmerke, og ikke modell utifra valg i combobox
+        'Dim sykkelnavnMerke As String() = sykkelnavn.Split(" ") 'sykkelnavnMerke(0) angir kun første ordet i setiningen
+        'MsgBox(sykkelnavn.Substring(0, sykkelnavn.IndexOf(" ")))
+
+        'sykkelnavn.Substring(0, sykkelnavn.IndexOf(" "))
+
+        Dim sql As String = "Update pdk_sykkel SET statusID='" & sqlHjelp & "'" & "WHERE merke='" & sykkelnavn.Substring(0, sykkelnavn.IndexOf(" ")) & "';"
+        data = query(sql)
+        'Hjelp
+
+
+
 
     End Sub
 
