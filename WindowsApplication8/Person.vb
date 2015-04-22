f7111750ae4983db7b5493992986f9b93f0a6b1f
thomasalf <thalfh'gmail.com>
2015-04-22 19:37:52 +0200

    Oppdatert klasser

--- WindowsApplication8/Person.vb
+++ WindowsApplication8/Person.vb
@@ -9,109 +9,136 @@
 
 
     'Konstruktører/Constructors
-    Public Sub New(ByVal etFornavn As String, _
-                   etEtternavn As String, _
-                   enTelefon As String, _
-                   enAdresse As String, _
-                   enEpost As String)
-        fornavn = etFornavn
-        etternavn = etEtternavn
-        telefon = enTelefon
-        adresse = enAdresse
-        epost = enEpost
+
+    'Setter alle verdier
+    Public Sub New(ByVal fn As String, _
+                   en As String, _
+                   tlf As String, _
+                   pnr As String, _
+                   adr As String, _
+                   ep As String)
+        setFornavn(fn)
+        setEtternavn(en)
+        setAdresse(adr)
+        setPostnummer(pnr)
+        setEpost(ep)
+        setTelefon(tlf)
     End Sub
 
-    Public Sub New(ByVal etFornavn As String, _
-                   etEtternavn As String)
-        fornavn = etFornavn
-        etternavn = etEtternavn
+    'Setter navn og telefonnummer
+    Public Sub New(ByVal fn As String, _
+                   en As String, _
+                   tlf As String)
+        setFornavn(fn)
+        setEtternavn(en)
+        setTelefon(tlf)
     End Sub
 
-    Public Sub New(ByVal etFornavn As String, _
-                   etEtternavn As String, _
-                   enTelefon As String, _
-                   enEpost As String)
-        fornavn = etFornavn
-        etternavn = etEtternavn
-        telefon = enTelefon
-        epost = enEpost
+    'Setter navn og e-post
+    Public Sub New(ByVal fn As String, _
+                   en As String, _
+                   ep As String)
+        setFornavn(fn)
+        setEtternavn(en)
+        setEpost(ep)
     End Sub
 
-        'Get- og set-funksjoner
-        Public Property Fornavnet() As String
-            Get
-                Return fornavn
-            End Get
-            Set(ByVal value As String)
-                fornavn = value
-            End Set
-        End Property
 
 
-        Public Property Etternavnet() As String
-            Get
-                Return etternavn
-            End Get
-            Set(ByVal value As String)
-                etternavn = value
-            End Set
-        End Property
+    'Get- og set-funksjoner
+    Public Function getFornavn() As String
+        Return fornavn
+    End Function
 
+    Public Sub setFornavn(ByVal fn As String)
+        If fn.Length > 0 Then 'sjekker at fornavnet er skrevet inn
+            fornavn = fn
+        Else
+            Throw New Exception("Ugyldig fornavn")
+        End If
 
-        Public Property Adressen() As String
-            Get
-                Return adresse
-            End Get
-            Set(ByVal value As String)
-                adresse = value
-            End Set
-        End Property
+    End Sub
 
 
 
 
-        Public Property Telefonen() As String
-            Get
-                Return telefon
-            End Get
-            Set(ByVal value As String)
-                If IsNumeric(value) And value.length = 8 Then
-                    telefon = value
-                Else
+
+
+    'Get- og set-funksjoner
+    Public Property Fornavnet() As String
+        Get
+            Return fornavn
+        End Get
+        Set(ByVal value As String)
+            fornavn = value
+        End Set
+    End Property
+
+
+    Public Property Etternavnet() As String
+        Get
+            Return etternavn
+        End Get
+        Set(ByVal value As String)
+            etternavn = value
+        End Set
+    End Property
+
+
+    Public Property Adressen() As String
+        Get
+            Return adresse
+        End Get
+        Set(ByVal value As String)
+            adresse = value
+        End Set
+    End Property
+
+
+
+
+    Public Property Telefonen() As String
+        Get
+            Return telefon
+        End Get
+        Set(ByVal value As String)
+            If IsNumeric(value) And value.Length = 8 Then
+                telefon = value
+            Else
                 Throw New Exception("Ugyldig telefonnummer")
-                End If
-            End Set
-        End Property
+            End If
+        End Set
+    End Property
 
 
-        Public Property Eposten() As String
-            Get
-                Return epost
-            End Get
-            Set(ByVal value As String)
-                If value.Contains("@") And value.Contains(".") Then
-                    epost = value
-                Else
+    Public Property Eposten() As String
+        Get
+            Return epost
+        End Get
+        Set(ByVal value As String)
+            If value.Contains("@") And value.Contains(".") Then
+                epost = value
+            Else
                 Throw New Exception("Ugyldig e-postadresse")
-                End If
-            End Set
-        End Property
-
-
-        
+            End If
+        End Set
+    End Property
 
 
 
 
-        '    Public Sub New(ByVal etfornavn As String, _
-        '                   etEtternavn As String, _
-        '                   enAdresse As String)
-        '        fornavn = etfornavn
-        '        etternavn = etEtternavn
-        '        adresse = enAdresse
-        '    End Sub
 
-        'Tostring()-funksjon
+
+
+    '    Public Sub New(ByVal etfornavn As String, _
+    '                   etEtternavn As String, _
+    '                   enAdresse As String)
+    '        fornavn = etfornavn
+    '        etternavn = etEtternavn
+    '        adresse = enAdresse
+    '    End Sub
+
+    'Tostring()-funksjon
     'Public Overrides Function ToString() As String
     '   Return fornavn & " " & etternavn & ". " & "Født: " & fødselsdato & " Adresse: " & adresse & " Telefon: " & _
     '      telefon & "E-post: " & epost
