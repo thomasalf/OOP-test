6bc34ff4b94c4a2be126f6ece43ffb321812638e
tovestoedle <tove_69@hotmail.com>
2015-04-30 08:28:44 +0200

    Statistikk

--- WindowsApplication8/Statistikk.vb
+++ WindowsApplication8/Statistikk.vb
@@ -1,14 +1,4 @@
-﻿Imports MySql.Data.MySqlClient
-Imports System.Configuration
+﻿Public Class Statistikk
 
-Public Class Statistikk
-
-    Public Function visBestillingerSQL(utdata As String)
-        Dim sql As String = "SELECT b.bookingID, b.uttid, b.inntid,b.kundeID, " _
-        & "CONCAT( k.kfornavn,  ' ', k.ketternavn) AS kunde, b.betalt," _
-        & "CONCAT( s.fornavn,  ' ', s.etternavn) AS selger FROM pdk_booking b," _
-        & "pdk_kunde k, pdk_ansatt s WHERE b.kundeID = k.kundeID and b.selgerID = s.selgerID;"
-        Return sql
-    End Function
 
 End Class
---
+++ WindowsApplication8/StatistikkDAO.vb
@@ -0,0 +1,16 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Configuration
+
+Public Class StatistikkDAO
+
+
+    Public Function visBestillingerSQL(utdata As String)
+        Dim sql As String = "SELECT b.bookingID, b.uttid, b.inntid,b.kundeID, " _
+        & "CONCAT( k.kfornavn,  ' ', k.ketternavn) AS kunde, b.betalt," _
+        & "CONCAT( s.fornavn,  ' ', s.etternavn) AS selger FROM pdk_booking b," _
+        & "pdk_kunde k, pdk_ansatt s WHERE b.kundeID = k.kundeID and b.selgerID = s.selgerID;"
+        Return sql
+    End Function
+
+
+End Class
