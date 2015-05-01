328d1acf9d16a15633a0f3c11a3565d15370c8e1
thomasalf <thalfh'gmail.com>
2015-05-01 16:01:46 +0200

    Forenklet kode for comboboxfylling

--- WindowsApplication8/ComboBoxUtil.vb
+++ WindowsApplication8/ComboBoxUtil.vb
@@ -1,47 +1,68 @@
 ﻿Public Class ComboBoxUtil
 
-    Public Sub fyllComboBoxMedSykkelmerke(comboboxnavn As ComboBox)
+    Public Sub fyllCombobox1(comboboxnavn As ComboBox, tabellnavn As String, radnavn As String)
         Dim sykkeldao As New SykkelDAO
         comboboxnavn.Items.Clear() 'Fjerner gammel informasjon fra combobox
         Dim data As New DataTable
-        Dim sql As String = "SELECT * FROM pdk_sykkelmerke"
+        Dim sql As String = "SELECT * FROM " & tabellnavn & ";"
         data = sykkeldao.query(sql)
 
-
-        If data.Rows.Count >= 1 Then 'Fyller combobox med merkeinformasjon
+        If data.Rows.Count >= 1 Then 'Fyller combobox med informasjon
             Dim teller As Integer
             teller = data.Rows.Count
 
             For teller = 0 To (teller - 1)
                 Dim ComboboxTekst As String
                 Dim row As DataRow = data.Rows(teller)
-                ComboboxTekst = row("merke")
+                ComboboxTekst = row(radnavn)
                 comboboxnavn.Items.Add(ComboboxTekst)
             Next
         End If
     End Sub
 
-    Public Sub fyllComboboxMedSykkelmodell(comboboxnavn As ComboBox)
-        Dim sykkeldao As New SykkelDAO
-        comboboxnavn.Items.Clear() 'Fjerner gammel informasjon fra combobox
-        Dim data As New DataTable
-        Dim sql As String = "SELECT * FROM pdk_sykkelmodell"
-        data = sykkeldao.query(sql)
+    '   Public Sub fyllComboBoxMedSykkelmerke(comboboxnavn As ComboBox)
+    ' Dim sykkeldao As New SykkelDAO
+    '     comboboxnavn.Items.Clear() 'Fjerner gammel informasjon fra combobox
+    ' Dim data As New DataTable
+    ' Dim sql As String = "SELECT * FROM pdk_sykkelmerke"
+    '     data = sykkeldao.query(sql)
+    '
+    '
+    '        If data.Rows.Count >= 1 Then 'Fyller combobox med merkeinformasjon
+    '    Dim teller As Integer
+    '            teller = data.Rows.Count
+    '
+    '            For teller = 0 To (teller - 1)
+    '    Dim ComboboxTekst As String
+    '    Dim row As DataRow = Data.Rows(teller)
+    '                ComboboxTekst = row("merke")
+    '                comboboxnavn.Items.Add(ComboboxTekst)
+    '            Next
+    '        End If
+    '    End Sub
+
+    ' Public Sub fyllComboboxMedSykkelmodell(comboboxnavn As ComboBox)
+    'Dim sykkeldao As New SykkelDAO
+    '    comboboxnavn.Items.Clear() 'Fjerner gammel informasjon fra combobox
+    'Dim data As New DataTable
+    'Dim sql As String = "SELECT * FROM pdk_sykkelmodell"
+    '    data = sykkeldao.query(sql)
+    '
+    '
+    '        If data.Rows.Count >= 1 Then 'Fyller combobox med modellinformasjon
+    '    Dim teller As Integer
+    '            teller = data.Rows.Count
+    '
+    '            For teller = 0 To (teller - 1)
+    '    Dim ComboboxTekst As String
+    '    Dim row As DataRow = Data.Rows(teller)
+    '                ComboboxTekst = row("modell")
+    '                comboboxnavn.Items.Add(ComboboxTekst)
+    '            Next
+    '        End If
+    '    End Sub
+
+    
 
 
-        If data.Rows.Count >= 1 Then 'Fyller combobox med modellinformasjon
-            Dim teller As Integer
-            teller = data.Rows.Count
-
-            For teller = 0 To (teller - 1)
-                Dim ComboboxTekst As String
-                Dim row As DataRow = data.Rows(teller)
-                ComboboxTekst = row("modell")
-                comboboxnavn.Items.Add(ComboboxTekst)
-            Next
-        End If
-    End Sub
-
-
-   
 End Class
