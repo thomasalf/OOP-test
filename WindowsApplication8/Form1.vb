d84214d82bfaa643e49c5875302de6dd02dd79a0
tovestoedle <tove_69@hotmail.com>
2015-05-01 20:43:46 +0200

    lagt til pris i statistikk

--- WindowsApplication8/Form1.vb
+++ WindowsApplication8/Form1.vb
@@ -437,7 +437,7 @@ Public Class Form1
         'flytte oppkobling til db over til StatistikkDAO
         Dim sql As String = "SELECT b.bookingID, b.uttid, b.inntid, b.kundeID, " _
         & "CONCAT( k.kfornavn,  ' ', k.ketternavn) AS kunde, b.betalt," _
-        & "CONCAT( s.fornavn,  ' ', s.etternavn) AS selger FROM pdk_booking b," _
+        & "CONCAT( s.fornavn,  ' ', s.etternavn) AS selger, b.pris FROM pdk_booking b," _
         & "pdk_kunde k, pdk_ansatt s WHERE b.kundeID = k.kundeID and b.selgerID = s.selgerID;"
 
         bestillinger = query(sql)
